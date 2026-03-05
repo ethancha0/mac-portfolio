6ea5e860494b0bc9cca4d08af906a27036369969
ethancha0 <ethanchao2005@gmail.com>
2026-03-05 15:10:39 -0800

    feat: navbar with time

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <h1 className="">Welcome to my MacFolio</h1>  
+  
 
-
+      <Navbar/>
 
 
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { navIcons, navLinks } from '../constants'
+
+import dayjs from "dayjs";
+
+
+const Navbar = () => {
+  return (
+
+    <nav>
+        <div>
+            <img src="/images/logo.svg"/>
+            <p className="font-bold">Ethan's Portfolio</p>
+
+            <ul>
+                { navLinks .map(({id, name }) => (
+                    <li key={id}>
+                        <p>{name}</p>
+
+                    </li>
+                ))}
+            </ul>
+        </div>
+
+        <div>
+            <ul>
+                {navIcons.map(({id, img})=>(
+                    <li key={id}>
+                        <img src={img} className="icon-hover"
+                        alt={`icon-${id}`}/>
+                    </li>
+                ))}
+
+                <time>{dayjs().format("ddd MMM D h:mm A")} </time>
+
+            </ul>
+        </div>
+
+    </nav>
+
+
+
+
+  )
+}
+
+export default Navbar
