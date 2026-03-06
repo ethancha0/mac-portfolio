eaceff532c2f82cad1e0c63ca783820c00ed730e
ethancha0 <ethanchao2005@gmail.com>
2026-03-05 17:15:01 -0800

    feat: welcome animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Welcome from './components/Welcome'
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
   
 
       <Navbar/>
+      <Welcome/>
 
 
 
