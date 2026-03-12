3df89a8bb6f56b985322f796d29bb445c377b874
ethancha0 <ethanchao2005@gmail.com>
2026-03-12 11:32:59 -0700

    feat: higher order component, open,close, draggable for terminal

--- src/hoc/WindowWrapper.tsx
+++ src/hoc/WindowWrapper.tsx
@@ -3,6 +3,7 @@ import gsap from 'gsap';
 import { Draggable } from 'gsap/all';
 import useWindowStore from '../store/window';
 import type { WindowKey } from '../store/window';
+import { useGSAP } from '@gsap/react';
 
 gsap.registerPlugin(Draggable);
 
@@ -11,9 +12,36 @@ const WindowWrapper = <P extends object,>(
   windowKey: WindowKey
 ) => {
   const Wrapped = (props: P) => {
-    const { windows } = useWindowStore();
+    const { focusWindow, windows } = useWindowStore();
     const { isOpen, zIndex } = windows[windowKey];
     const ref = useRef<HTMLElement | null>(null);
+
+    useGSAP(() =>{
+      const el = ref.current;
+      if(!el || !isOpen) return;
+
+      el.style.display = "block";
+
+      gsap.fromTo(
+          el,
+         { scale: 0.8, opacity: 0, y:40},
+         { scale: 1, opacity: 1, y: 0, duration: 0.4, ease: 
+          "power3.out"},
+         );
+        }, [isOpen]);
+
+    useGSAP(()=>{
+      const el = ref.current;
+      if (!el) return;
+      const [instance] = Draggable.create(el,
+         { onPress: () => focusWindow(windowKey)
+         })
+
+      return () => instance.kill()
+
+    },[]);
+
+
 
     useLayoutEffect(() => {
       const el = ref.current;
--- src/windows/Terminal.tsx
+++ src/windows/Terminal.tsx
@@ -1,12 +1,13 @@
 import WindowWrapper from "../hoc/WindowWrapper"
 import {Check, Flag} from "lucide-react"
 import { techStack } from "../constants"
+import WindowControls from "../components/WindowControls"
 
 const Terminal = () => {
   return (
     <>
     <div id="window-header">
-      <p>Window Controls</p>
+      <WindowControls target="terminal"/>
       <h2>Tech Stack</h2>
     </div>
 
