57f2cc6729a93b1ef99a0ee10ece726fa092bf1d
Ethan Chao <ethanchao2005@gmail.com>
2026-03-12 11:58:30 -0700

    Highter order component secondhalf (#3)
    * feat: fixed toggle terminal
    
    * feat: higher order component, open,close, draggable for terminal
    
    * fix: props

---
+++ src/components/WindowControls.tsx
@@ -0,0 +1,18 @@
+import useWindowStore, { type WindowKey } from "../store/window";
+
+
+const WindowControls = ({target}: {target: WindowKey}) => {
+    const { closeWindow } = useWindowStore();
+
+
+  return (
+    <div id="window-controls">
+        <div className="close" onClick={() => closeWindow(target)} />
+        <div className="minimize"/>
+        <div className="maximize"/>
+      
+    </div>
+  )
+}
+
+export default WindowControls
--- src/windows/Terminal.tsx
+++ src/windows/Terminal.tsx
@@ -1,10 +1,13 @@
 import WindowWrapper from "../hoc/WindowWrapper"
+import {Check, Flag} from "lucide-react"
+import { techStack } from "../constants"
+import WindowControls from "../components/WindowControls"
 
 const Terminal = () => {
   return (
     <>
     <div id="window-header">
-      <p>Window Controls</p>
+      <WindowControls target="terminal"/>
       <h2>Tech Stack</h2>
     </div>
 
@@ -13,6 +16,43 @@ const Terminal = () => {
         <span className="font-bold">@ethan %</span>
         show tech stack
       </p>
+
+      <div className="label">
+        <p className ="w-32">Category</p>
+        <p>Technologies</p>
+      </div>
+    
+      <ul className="content">
+        {techStack.map(({category, items}) =>(
+          <li key={category} className="flex items-center">
+              <Check className="check" size={20}/>
+              <h3>{category}</h3>
+              <ul>
+                {items.map((item, i) => (
+                  <li key={i}>{item}
+                  { i< items.length - 1 ? ',' : ""}</li>
+                ))}
+              </ul>
+          </li>
+
+        ))}
+
+      </ul>
+
+      <div className="footnote">
+        <p>
+          <Check size={20} /> 5 of 5 stacks loaded successfully (100%)
+        </p>
+        <p className="text-black">
+          <Flag size={15} fill="black"/>
+          Render Time: 14ms
+        </p>
+
+      </div>
+
+
+
+
     </div>
 
     </>
